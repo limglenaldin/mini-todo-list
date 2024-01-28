75423581aee72f4f77018a92635b072cd6c5f08e
Glenaldin Halim <glenaldinhalim16@gmail.com>
2024-01-29 03:36:06 +0700

    scaffolding the project

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
 public class App {
+    /**
+     * Repository Layer
+     */
+    public static String[] todos = new String[10];
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+
+    /**
+     * Service Layer
+     */
+    public static void show() {
+        // implementation
+    }
+
+    public static void add() {
+        // implementation
+    }
+
+    public static void remove() {
+        // implementation
+    }
+
+    /**
+     * Presentation Layer
+     */
+    public static void viewShow() {
+        // implementation
+    }
+
+    public static void viewAdd() {
+        // implementation
+    }
+
+    public static void viewRemove() {
+        // implementation
     }
 }
